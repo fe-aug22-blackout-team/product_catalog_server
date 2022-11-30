d3d3fc692e52011131f94812f7ecccfa3561496a
Vadym Mamchur <vmchr344@gmail.com>
2022-11-30 11:50:44 +0200

    feat: products endpoint has been created

---
+++ src/types/Product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  age: number,
+  carrier?: string,
+  id: string,
+  type: string,
+  imageUrl: string,
+  name: string,
+  snippet: string,
+  price: number,
+  discount: number,
+  screen: string,
+  capacity: string,
+  ram: string,
+}
